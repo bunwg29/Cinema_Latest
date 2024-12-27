0ad0d47446f6681d2b709829dad8dc4177fec2cb
Bun <bun2932005@gmail.com>
2024-12-27 12:46:07 +0700

    style: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
-    <p className="font-bold">Movie App</p>
-  )
+    <div>
+      <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
+        <div className="flex items-center gap-4">
+          <img src="./netflix.png" alt="netflix" className="w-16 sm:w-28" />
+          <a href="#">Phim</a>
+          <a href="#">Truyền hình</a>
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
