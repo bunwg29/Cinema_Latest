4147d006b4ecf73b990b60037a8495e13828aa1b
Bun <bun2932005@gmail.com>
2024-12-28 14:23:44 +0700

    style: slider of feature lists

--- src/components/FeatureMovies/Movie.jsx
+++ src/components/FeatureMovies/Movie.jsx
@@ -1,46 +1,42 @@
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Movie() {
+const Movie = (props) => {
+
+  const { data: { backdrop_path, title, release_date, overview } } = props;
+  
   return (
     <div>
       <img
-        src="https://starlight.vn/Areas/Admin/Content/Fileuploads/images/Tintuc/inside-2.jpg"
-        alt=""
-        className="aspect-video w-full brightness-50"
+        src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+        className="aspect-video brightness-50"
       />
       <div className="absolute bottom-[10%] left-8 w-1/2 sm:w-1/3">
-        <p className="mb-2 font-bold sm:text-[2vw]">Inside Out 2</p>
+        <p className="mb-2 font-bold sm:text-[2vw]">{title}</p>
         <div>
-          <p className="mb-1 inline-block border border-gray-400 p-1 text-gray-400">
+          {/* <p className="mb-1 inline-block border border-gray-400 p-1 text-gray-400">
             PG13
-          </p>
-          <p className="text-[1.2vw]">2024-06-11</p>
+          </p> */}
+          <p className="text-[1.2vw]">{release_date}</p>
         </div>
         <div>
           <div className="mt-4 hidden text-[1.2vw] sm:block">
             <p className="mb-2 font-bold">Overview</p>
             <p>
-              Teenager Riley&apos;s mind headquarters is undergoing a sudden
-              demolition to make room for something entirely unexpected: new
-              Emotions! Joy, Sadness, Anger, Fear and Disgust, who&apos;ve long
-              been running a successful operation by all accounts, aren&apos;t
-              sure how to feel when Anxiety shows up. And it looks like
-              she&apos;s not alone.
+              {overview}
             </p>
           </div>
-        </div>
-        <div className="mt-4">
-          <button className="text-10 mr-2 rounded bg-white px-4 py-2 text-black lg:text-lg">
-            <FontAwesomeIcon icon={faPlay} />
-            Trailer
-          </button>
-          <button className="text-10 rounded bg-slate-300/35 px-4 py-2 lg:text-lg">
-            View Detail
-          </button>
+          <div className="mt-4">
+            <button className="text-10 mr-2 rounded bg-white px-4 py-2 text-black lg:text-lg">
+              <FontAwesomeIcon icon={faPlay} /> Trailer
+            </button>
+            <button className="text-10 rounded bg-slate-300/35 px-4 py-2 lg:text-lg">
+              View Detail
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 export default Movie;
