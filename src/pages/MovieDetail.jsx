31ff3a6ff7a46c47ea709327b775ceee8d6b093e
Bun <bun2932005@gmail.com>
2025-02-07 16:18:30 +0700

    refactor: Add title for RelatedMediaList component

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -53,12 +53,13 @@ function MovieDetail() {
         }
       />
       <div className="bg-black text-[1.2vw] text-white">
-        <div className="mx-auto flex max-w-screen-xl gap-6 px-6 py-10 sm:gap-8">
+        <div className="container">
           <div className="flex-[2]">
             <ActorList actors={movieInfo.credits?.cast || []} />
             <RelatedMediaList
               mediaList={relatedMovies}
               isLoading={isRelatedMovieLoading}
+              title="More like this"
             />
           </div>
           <div className="flex-1">
